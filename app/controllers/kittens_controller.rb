5c9cf84a48bb457a965d453d627340825739a88e
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-06-03 12:57:36 +0100

    DOne with kitten view and actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,26 @@
 class KittensController < ApplicationController
+  include KittensHelper
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @kittens }
+      format.json { render :json => @kittens}
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+  def new
+    @kitten = Kitten.new
+  end
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
 end
